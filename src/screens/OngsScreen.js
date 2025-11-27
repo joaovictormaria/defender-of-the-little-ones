3167f2c7b8d3d6409d6d2a4bead567b81f213299
joaovictormaria <joaovictormaria33@gmail.com>
2025-11-27 18:47:55 -0300

    feat: adicionando a tela de ongs em aracaju

--- src/screens/OngsScreen.js
+++ src/screens/OngsScreen.js
@@ -1,22 +1,64 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Linking } from "react-native";
+import BackButton from "../components/BackButton";
 
 export default function OngsScreen({ navigation }) {
+
+  const abrirSite = (url) => {
+    Linking.openURL(url);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>ONGs próximas</Text>
-      <Text style={styles.text}>Aqui serão listadas ONGs da sua região.</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
-        <Text style={styles.buttonText}>Voltar</Text>
+      <BackButton navigation={navigation}/>
+      <Text style={styles.title}>ONGs em Aracaju</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => abrirSite("https://www.gpaa.com.br")}
+      >
+        <Text style={styles.buttonText}>GPAA - Grupo de Proteção Animal</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => abrirSite("https://www.instagram.com/protecaoanimalaracaju")}
+      >
+        <Text style={styles.buttonText}>Proteção Animal Aracaju</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => abrirSite("https://www.instagram.com/amigosdosanimaisaju")}
+      >
+        <Text style={styles.buttonText}>Amigos dos Animais Aju</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => abrirSite("https://www.instagram.com/projetoamparoanimal")}
+      >
+        <Text style={styles.buttonText}>Projeto Amparo Animal</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => abrirSite("https://www.instagram.com/sosanimaisaracaju")}
+      >
+        <Text style={styles.buttonText}>SOS Animais Aracaju</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  title: { fontSize: 22, fontWeight: "bold", color: "#b00000" },
-  text: { marginVertical: 20 },
-  button: { backgroundColor: "#b00000", padding: 10, borderRadius: 10 },
-  buttonText: { color: "#fff", fontWeight: "bold" },
+  container: { flex: 1, justifyContent: "center", alignItems: "center", padding: 20 },
+  title: { fontSize: 26, fontWeight: "bold", color: "#b00000", marginBottom: 20 },
+  button: {
+    backgroundColor: "#b00000",
+    padding: 12,
+    borderRadius: 10,
+    marginVertical: 8,
+    width: "100%",
+  },
+  buttonText: { color: "#fff", fontWeight: "bold", textAlign: "center" },
 });
